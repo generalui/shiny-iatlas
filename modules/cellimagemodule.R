20750f71c8342b29195a0bb1066d93cb23f835e4
Vésteinn Þórsson <thorsson@systemsbiology.org>
2019-04-04 06:09:56 -0700

    beginning work on color range

--- modules/cellimagemodule.R
+++ modules/cellimagemodule.R
@@ -2,6 +2,24 @@
 library(grImport2)
 library(grid)
 library(feather)
+library(tidyverse)
+library(RColorBrewer) 
+
+## Colors - working area
+gmin <- -0.25
+gmax <- 0.25
+gnstep <- 51
+gstep <- (gmax-gmin)/(gnstep-1) ## size of step 
+##gstep <- 0.01
+breakList <- seq(gmin,gmax,gstep) 
+allcolors <- colorRampPalette(rev(brewer.pal(n = 7,name="RdBu")))(length(breakList))
+
+b <- 0.1457
+cind <- min(which(!(b-breakList)>0)) ## right turnover point
+
+usecolor <- allcolors[cind]
+
+##display.brewer.pal(n=7,name="RdBu")
 
 ## Data
 im_expr_df <- read_feather('data/im_expr_df.feather')
@@ -16,16 +34,52 @@ path.annotations <- read.table('data/PathIDs.txt',as.is=T)$V1
 
 tumortype <- "SKCM"
 
-## Gene averages
+## General gene averages and extrema
+dfe <- im_expr_tt_df %>% group_by(Symbol,Study) %>% summarize(mean_exp=mean(normalized_count,na.rm=T)) ## averages for all genes and all types
+min.per.gene <- dfe %>% group_by(Symbol) %>% summarize(gene_min=min(mean_exp,na.rm=T))
+max.per.gene <- dfe %>% group_by(Symbol) %>% summarize(gene_max=max(mean_exp,na.rm=T))
+
+## specific values for the choice
 image.ids <- variable.annotations %>% filter(Source=="im_expr_df") %>% select(ImageVariableID) %>% as_vector() %>% as.character()
 feature.ids <- variable.annotations %>% filter(Source=="im_expr_df") %>% select(FeatureLabel) %>% as_vector() %>% as.character()
-gene.vals <- im_expr_tt_df %>% filter(Study==tumortype,Symbol %in% feature.ids) %>% select(Symbol,normalized_count) %>% group_by(Symbol) %>% summarize(mean_exp=mean(normalized_count))
+gene.vals <- im_expr_tt_df %>% filter(Study==tumortype,Symbol %in% feature.ids) %>% select(Symbol,normalized_count) %>% 
+              group_by(Symbol) %>% summarize(mean_exp=mean(normalized_count))
+gene.vals <- dfe %>% filter(Study==tumortype,Symbol %in% feature.ids)
 
-## Cell averages
+## Compute colors for the specific choice
+min.per.gene %>% filter(Symbol %in% feature.ids)
+max.per.gene %>% filter(Symbol %in% feature.ids)
+
+gnstep <- 51
+allcolors <- colorRampPalette(rev(brewer.pal(n = 7,name="RdBu")))(length(breakList))
+plotcolors <- character(length=length(feature.ids)) ; names(plotcolors) <- feature.ids
+
+for ( f in feature.ids) {
+  gmin <- min.per.gene %>% filter(Symbol==f) %>% .$gene_min
+  gmax <- max.per.gene %>% filter(Symbol==f) %>% .$gene_max
+  gstep <- (gmax-gmin)/(gnstep-1) ## size of step 
+  breakList <- seq(gmin,gmax,gstep) 
+  b <- gene.vals %>% filter(Symbol==f) %>% .$mean_exp
+  cind <- min(which(!(b-breakList)>0)) ## right turnover point
+  plotcolors[f] <- allcolors[cind]
+}
+
+## Cell averages - general and extrema
+
+means.per.cell <- fmx_df %>% select(Study,feature.ids) %>% group_by(Study) %>% summarize(cell_mean=mean(T_cells_CD8.Aggregate2,na.rm=T))
+# above not sure if we need wrapr::let . See transform.R under functions 
+
 image.ids <- variable.annotations %>% filter(Source=="fmx_df") %>% select(ImageVariableID) %>% as_vector() %>% as.character()
 feature.ids <- variable.annotations %>% filter(Source=="fmx_df") %>% select(FeatureLabel) %>% as_vector() %>% as.character()
 
-df <- fmx_df %>% filter(Study==tumortype) %>% select() ## need to select feature.ids  
+df <- fmx_df %>% filter(Study==tumortype) %>% select(feature.ids) ## need to select feature.ids  
+cell.vals <- mean(df[[feature.ids]],na.rm = T) ## will need to change when there are more cells
+
+## WORK HERE NEXT
+cmin <- min(means.per.cell$cell_mean)
+cmax <- max(means.per.cell$cell_mean)
+
+
 
 ## Get image and convert to grid object
 pic <- grImport2::readPicture("data/tcell-start-cairo-edited.svg")
